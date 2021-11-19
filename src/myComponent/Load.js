c9d0245bd4a3e308934a283abd3f7d6bf30bb18d
Ruhan-Ahmad <ruhanff786@gmail.com>
2021-11-19 19:43:11 +0500

    v1.1

--- src/myComponent/Load.js
+++ src/myComponent/Load.js
@@ -4,7 +4,7 @@ import { Show } from './Show'
 export const Load = () => {
     const [value, setvalue] = useState([])
   let res = async() =>{
-    let result = await fetch('/api')
+    let result = await fetch('https://flask-react-app1.herokuapp.com/api')
     let data = await result.json()
     setvalue(data.name)
   }
